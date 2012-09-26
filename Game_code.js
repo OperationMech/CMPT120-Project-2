3bd381b66cdedf6f3b6d3447aa1aa92668d6217c
OperationMech <operationmech@gmail.com>
2012-09-25 21:45:28 -0400

    Added comments updated text.

--- Game_code.js
+++ Game_code.js
@@ -1,9 +1,11 @@
 // Javascript
 
+//Global variables
 var locations = [false, false, false, false];
 var score = 0;
 
 function Change_location(dir) {
+  //Buttton to location conversion
     var game_area = document.getElementById("Game_area");
 	var direction = "";
     if(	dir === 1) {
@@ -19,29 +21,32 @@ function Change_location(dir) {
 	  direction = "west";
 	  move_to_Area(game_area, direction);
 	} else {
-	  alert("This should not happen");
+	  alert("This should not happen"); // ERROR HANDLING
 	}
 }
 
+// No more placeholders
 function move_to_Area(game_area, newLocation) {
+  // Chooses direction
     if( newLocation === "north") {
-	   game_area.value = "New North area" + "\n\n" + game_area.value;
+	   game_area.value = "You hit the panel wall." + "\n\n" + game_area.value;
 	   Increase_score_once(newLocation);
 	}else if( newLocation === "south") {
-	   game_area.value = "New South area" + "\n\n" + game_area.value;
+	   game_area.value = "You ran into the south pane." + "\n\n" + game_area.value;
 	   Increase_score_once(newLocation);
 	}else if( newLocation === "east") {
-	   game_area.value = "New East area" + "\n\n" + game_area.value;
+	   game_area.value = "You walked into the east pane." + "\n\n" + game_area.value;
 	   Increase_score_once(newLocation);
 	}else if( newLocation === "west") {
-	   game_area.value = "New West area" + "\n\n" + game_area.value;
+	   game_area.value = "You tapped the west pane." + "\n\n" + game_area.value;
 	   Increase_score_once(newLocation);
 	}else {
-	  alert( "This should never happen");
+	  alert( "This should never happen"); // ERROR HANDLING
 	}
 }
 
-function Increase_score_once(location_visited) {
+function Increase_score_once(location_visited /*,pair - future use */) {
+  //scoring function and logical check
    var score_area = document.getElementById("score_print");
    if( location_visited ===  "north" && locations[0] === false) {
        locations[0] = true;
@@ -55,8 +60,8 @@ function Increase_score_once(location_visited) {
 	}else if( location_visited ===  "west" && locations[3] === false) {
        locations[3] = true;
 	   score += 5;
-	}else {
-	  alert( "Visited"); // was for error handling now just a feature
-	}
+	}/*else {
+	  alert( "Visited");  was for error handling now just a comment
+	}*/
 	score_area.value = "Score:" + score;
 }
